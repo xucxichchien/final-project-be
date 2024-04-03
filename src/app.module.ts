72201c2205c3e9125fa8c6fad3559be037e82042
xucxichchien <quenguyendan6@gmail.com>
2024-04-03 09:52:42 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
+
+@Module({
+  imports: [AuthModule, BookmarkModule, UserModule, PrismaModule, 
+    ConfigModule.forRoot({isGlobal: true})]
+
+})
+export class AppModule {}
+
